5124908d6c808a0a07681dfc7b47752af0f74767
Honza S. Copak <bonum.comunae@volny.cz>
2023-04-27 15:53:21 +0200

    add relay info - NIP-11

--- agama_nostr/tools.py
+++ agama_nostr/tools.py
@@ -11,3 +11,36 @@ def print_head(label="head"):
     print("-"*WIDTH)
     print("-"*5, label)
     print("-"*WIDTH)
+
+
+
+def get_relay_information(url: str, timeout: float = 2, add_url: bool = True):
+    # NIP-11 // 'pynostr'
+    import requests 
+    headers = {'Accept': 'application/nostr+json', 'User-Agent': 'agama_nostr'}
+    if "wss" in url:
+        metadata_uri = url.replace("wss", "https")
+    elif "ws" in url:
+        metadata_uri = url.replace("ws", "http")
+    else:
+        raise Exception(f"{url} is not a websocket url")
+    try:
+        response = requests.get(metadata_uri, headers=headers, timeout=timeout)
+
+        response.raise_for_status()
+
+        metadata = response.json()
+        if add_url:
+            metadata["url"] = url
+        return metadata
+    except requests.exceptions.Timeout:
+        # Handle a timeout error
+        print("Request timed out. Please try again later.")
+
+    except requests.exceptions.HTTPError as err:
+        # Handle an HTTP error
+        print(f"HTTP error occurred: {err}")
+
+    except requests.exceptions.RequestException as err:
+        # Handle any other request exception
+        print(f"An error occurred: {err}")
