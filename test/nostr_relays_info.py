9cdf7e69f016e2921ad6b440de9bfb2a6577fec6
Honza S. Copak <bonum.comunae@volny.cz>
2023-04-29 09:41:59 +0200

    added relays info data-list

--- test/nostr_relays_info.py
+++ test/nostr_relays_info.py
@@ -1,28 +1,37 @@
 import json
+from datetime import datetime
 from agama_nostr.tools import get_relay_information, print_head, short_str
 # from pynostr.relay_list import RelayList
-from agama_nostr.relays import relays_list, relays_big_list
+from agama_nostr.relays import relays_list, relays_big_list, nip_11_struct_adv
 
 
 DEBUG = True
+LOG = True
+
+logFile = "data/"+datetime.now().strftime("%Y%m%d_%H%M%Srelays.txt")
+
+def log_to_file(str, log=LOG):
+    print(str)
+    if log:
+        fLog = open(logFile,"a") #a
+        fLog.write(str+"\n")
+        fLog.close()
+
 
 # relay_url = "wss://relay.damus.io"
+log_to_file("relays INFO - ver.0.1/2023-04")
+log_to_file("="*39)
 
-"""
- "name": <string identifying relay>,
- "description": <string with detailed information>,
- "pubkey": <administrative contact pubkey>,
- "contact": <administrative alternate contact>,
- "supported_nips": <a list of NIP numbers supported by the relay>,
- "software": <string identifying relay software URL>,
- "version": <string version identifier>
-"""
-
-nip_11_struct = [ "name","description","pubkey","contact","supported_nips","software","version","limitation"]
 
 i = 1
-for relay_url in relays_list: # relays_big_list:
-    print_head(str(i) + ") " +relay_url)
+# for relay_url in relays_list: 
+for relay_url in relays_big_list:
+    h_str = str(i) + ") " +relay_url
+    #print_head(str)
+    log_to_file("-"*39)
+    log_to_file(h_str)
+    log_to_file("-"*39)
+    
     i += 1
     
     relay_data = get_relay_information(relay_url)
@@ -32,10 +41,11 @@ for relay_url in relays_list: # relays_big_list:
         print("relay_data:", relay_data)
         print("[relay_dict]")
     try:
-        for info in nip_11_struct:
+        for info in nip_11_struct_adv:
             #if info = "pubkey":
             #print("{:<11s} - {}".format(info,short_str(relay_data.get(info))))
-            print("{:<13s} - {}".format(info,str(relay_data.get(info))))
+            r_str = "{:<13s} - {}".format(info,str(relay_data.get(info)))
+            log_to_file(r_str)
 
         #relay_dict = json.loads(relay_data )
         #print(relay_dict)
@@ -43,6 +53,7 @@ for relay_url in relays_list: # relays_big_list:
             print("Err. parse relay_data")
     #except:
     #    print("Err. get_relay_information")
+
 
 """
 {'id': 'wss://nostr.mnethome.de/', 
