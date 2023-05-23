12fe28291da5b17ae692cbd95ea7652ec3d597ff
Honza S. Copak <bonum.comunae@volny.cz>
2023-05-23 13:11:51 +0400

    Update nostr_meta_info.py

--- apps/nostr_meta_info.py
+++ apps/nostr_meta_info.py
@@ -1,29 +1,29 @@
 import json
 from time import sleep
 from agama_nostr.client import Client 
-from agama_nostr.nostr_key import NOSTR_SEC
-from agama_nostr.tools import print_head
+from agama_nostr.tools import get_nostr_key, print_head
 from agama_nostr.nips import meta_struct
 
 
 DEBUG = True
-nostr_client = Client(NOSTR_SEC)
+NOSTR_SEC = get_nostr_key() 
+nc = Client(NOSTR_SEC) # nostr_client
 
 #print_head("my relays list")
-#nostr_client.print_myrelays_list()
+#nc.print_myrelays_list()
 
-#npub1 = "npub1.........user id.................... "
-npub1 = "npub1relay9mayryh7vnvmf5e250sskuw07fk2z6gkm585zltehlwhj9stx05lg"
+npub1 = "npub1.........user id.................... "
+#npub1 = "npub1relay9mayryh7vnvmf5e250sskuw07fk2z6gkm585zltehlwhj9stx05lg"
 
-nostr_client.set_filter_meta(npub1)
-nostr_client.set_subscription_id()
+nc.set_filter_meta(npub1)
+nc.set_subscription_id()
 
-nostr_client.single_relay_event()
-nostr_client.message_pool_events()
+nc.single_relay_event()
+nc.message_pool_events()
 
 if DEBUG:
     print("-"*39)
-    print(nostr_client.last_event_msg)
+    print(nc.last_event_msg)
     print("-"*39)
 meta_data = nostr_client.last_event_msg.content
 data = json.loads(meta_data)
